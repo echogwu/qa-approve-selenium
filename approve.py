052bf282b79740d015d5ada48ef9a21bd27d4db0
Echo Wu <echowu@lyft.com>
2021-08-10 16:50:43 -0700

    remove the credentials

--- approve.py
+++ approve.py
@@ -2,16 +2,14 @@ import time
 from selenium import webdriver
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
-
 driver.get('https://tom-staging.lyft.net/users/1529241897037654594/applicant');
 
 time.sleep(5) # Let the user actually see something!
-username = driver.find_elements_by_css_selector("#okta-signin-username")[0]
-username.send_keys("echowu@lyft.com")
-password = driver.find_elements_by_css_selector("#okta-signin-password")[0]
-password.send_keys("positiveattitude")
-password.submit()
 
-time.sleep(35)
+username = driver.find_elements_by_css_selector("#okta-signin-username")[0]
+username.send_keys("") // put your email address here
+password = driver.find_elements_by_css_selector("#okta-signin-password")[0]
+password.send_keys("") // put your okta password here
+password.submit()
 
 driver.quit()
